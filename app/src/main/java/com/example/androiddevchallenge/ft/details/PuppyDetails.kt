77d6bd901742cf33ed4a4bda52c443f21850739c
Lukasz <ukasz1412@gmail.com>
2021-02-28 17:26:20 +0000

    Details preview

--- app/src/main/java/com/example/androiddevchallenge/ft/details/PuppyDetails.kt
+++ app/src/main/java/com/example/androiddevchallenge/ft/details/PuppyDetails.kt
@@ -1,8 +1,15 @@
 package com.example.androiddevchallenge.ft.details
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun PuppyDetails() {
 
+}
+
+@Preview
+@Composable
+fun PuppyDetailsPreview() {
+    PuppyDetails()
 }
