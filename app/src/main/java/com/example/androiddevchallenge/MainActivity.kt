5b0273a21197ead2b129f852114bbb6b2543b32d
Luke <ukasz1412@gmail.com>
2021-03-03 00:43:54 +0000

    Status bar color

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -49,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
+                window.statusBarColor =MaterialTheme.colors.primary.toArgb() // fixme - ugly
                 val navController = rememberNavController()
                 NavHost(
                     navController = navController,
