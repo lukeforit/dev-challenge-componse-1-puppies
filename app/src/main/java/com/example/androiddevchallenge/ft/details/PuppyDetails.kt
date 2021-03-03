c2dbdb3fe3c17f0700426f21167555150cefe623
Luke <ukasz1412@gmail.com>
2021-03-03 00:08:50 +0000

    Display name and a button in details

--- app/src/main/java/com/example/androiddevchallenge/ft/details/PuppyDetails.kt
+++ app/src/main/java/com/example/androiddevchallenge/ft/details/PuppyDetails.kt
@@ -1,18 +1,23 @@
 package com.example.androiddevchallenge.ft.details
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.data.Puppy
 import dev.chrisbanes.accompanist.glide.GlideImage
 
 @Composable
 fun PuppyDetails(puppy: Puppy) {
-    Row() {
+    Column {
         GlideImage(
             data = puppy.img,
             contentDescription = "My content description",
@@ -22,6 +27,16 @@ fun PuppyDetails(puppy: Puppy) {
                 }
             },
         )
+
+        Text(
+            text = puppy.name,
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Button(onClick = { /*TODO*/ }, Modifier.padding(4.dp).align(Alignment.CenterHorizontally)) {
+            Text(text = "Take me home!")
+        }
     }
 }
 
