4d9ceec7b8038e32a31334d17e0144332e434e3b
Lukasz <ukasz1412@gmail.com>
2021-05-31 21:52:08 +0100

    Update dependencies to bumble bee AS

--- app/src/main/java/com/example/androiddevchallenge/ft/browse/BrowsePuppies.kt
+++ app/src/main/java/com/example/androiddevchallenge/ft/browse/BrowsePuppies.kt
@@ -48,7 +48,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 import com.example.androiddevchallenge.data.entity.Puppy
 import com.example.androiddevchallenge.data.entity.Trait
