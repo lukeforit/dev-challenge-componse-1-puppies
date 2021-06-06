9ec5ec9b4fc63a93e3544e08af4d4ea72207b772
Lukasz <ukasz1412@gmail.com>
2021-06-06 17:17:05 +0100

    Space between cards

--- app/src/main/java/com/example/androiddevchallenge/ft/browse/cmp/TraitsHorizontalGrid.kt
+++ app/src/main/java/com/example/androiddevchallenge/ft/browse/cmp/TraitsHorizontalGrid.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -21,7 +20,8 @@ fun TraitsHorizontalGrid(traits: List<Trait>) {
     LazyRow(
         modifier = Modifier.fillMaxHeight(),
         state = rememberLazyListState(),
-        contentPadding = PaddingValues(horizontal = 16.dp)
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(
             items = traits.zipWithNext(),
