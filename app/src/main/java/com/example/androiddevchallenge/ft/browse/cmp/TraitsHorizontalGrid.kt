5a018917f79781ccf60e2c2ab17e4b0001289736
Lukasz <ukasz1412@gmail.com>
2021-06-06 18:19:41 +0100

    Section title

--- app/src/main/java/com/example/androiddevchallenge/ft/browse/cmp/TraitsHorizontalGrid.kt
+++ app/src/main/java/com/example/androiddevchallenge/ft/browse/cmp/TraitsHorizontalGrid.kt
@@ -6,9 +6,13 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,24 +21,33 @@ import com.example.androiddevchallenge.data.entity.Trait
 
 @Composable
 fun TraitsHorizontalGrid(traits: List<Trait>) {
-    LazyRow(
-        modifier = Modifier.fillMaxHeight(),
-        state = rememberLazyListState(),
-        contentPadding = PaddingValues(horizontal = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(
-            items = traits.zipWithNext(),
-            itemContent = { itemScope ->
-                Column(
-                    verticalArrangement = Arrangement.SpaceBetween
-                ) {
-                    TraitCard(trait = itemScope.first)
-                    Spacer(modifier = Modifier.height(8.dp))
-                    TraitCard(trait = itemScope.second)
-                }
-            }
+    Column {
+        Text(
+            text = "Search by trait",
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier.padding(start = 16.dp)
         )
+        Spacer(Modifier.size(8.dp))
+        LazyRow(
+            modifier = Modifier.fillMaxHeight(),
+            state = rememberLazyListState(),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(
+                items = traits.zipWithNext(),
+                itemContent = { itemScope ->
+                    Column(
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        TraitCard(trait = itemScope.first)
+                        Spacer(modifier = Modifier.height(8.dp))
+                        TraitCard(trait = itemScope.second)
+                    }
+                }
+            )
+        }
+
     }
 }
 
