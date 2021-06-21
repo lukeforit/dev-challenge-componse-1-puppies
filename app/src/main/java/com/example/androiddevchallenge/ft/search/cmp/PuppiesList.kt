c56ed83f51efcbf0baae8881af0b33b1bdbb74f3
Lukasz <ukasz1412@gmail.com>
2021-06-21 18:30:43 +0100

    Remove sticky header

--- app/src/main/java/com/example/androiddevchallenge/ft/search/cmp/PuppiesList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ft/search/cmp/PuppiesList.kt
@@ -4,16 +4,10 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.data.entity.Puppy
@@ -33,25 +27,9 @@ fun PuppiesList(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = contentPadding,
     ) {
-        puppies
-            .sortedBy(Puppy::name)
-            .groupBy { it.name[0] }
-            .forEach { (character, puppiesGroup) ->
-                stickyHeader {
-                    Surface(color = MaterialTheme.colors.surface) {
-                        Text(
-                            text = character.toString(),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(4.dp),
-                            style = MaterialTheme.typography.subtitle1
-                        )
-                    }
-                }
-                items(items = puppiesGroup, key = Puppy::id) { puppy ->
-                    PuppyItem(puppy, openPuppyDetails)
-                }
-            }
+        items(items = puppies) { puppy ->
+            PuppyItem(puppy, openPuppyDetails)
+        }
     }
 }
 
